7aa87030e0c1b71e883ddb0fb28688c4bf85cdb1
Rafael Afonso <99260758+RafaelAfons0@users.noreply.github.com>
2023-11-25 15:26:43 -0300

    Add files via upload

---
+++ demo/demo/src/main/java/back/gs/fiap/com/DemoApplication.java
@@ -0,0 +1,13 @@
+package back.gs.fiap.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ demo/demo/src/test/java/back/gs/fiap/com/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package back.gs.fiap.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
